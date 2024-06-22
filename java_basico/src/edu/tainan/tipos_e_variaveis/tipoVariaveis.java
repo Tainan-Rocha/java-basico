e805df65725352ab4af7431408d3f9157d0bd108
Tainan-Rocha <tainan.estudo@gmail.com>
2024-06-22 17:00:13 -0300

    adicionando arquivo sobre tipo de variaveis do java

---
+++ java_basico/src/edu/tainan/tipos_e_variaveis/tipoVariaveis.java
@@ -0,0 +1,23 @@
+package edu.tainan.tipos_e_variaveis;
+
+// Tipos de dados
+
+
+public class tipoVariaveis {
+    public static void main(String[] args) {
+        byte numeroPequeno = 120; // Tipo "byte" | Valor Máximo - > 127
+        short ano = 2021; // Tipo "short" | Valor Máximo - > 32.767
+        int idade = 20000; // Tipo "int" | Valor Máximo - > 2.147.483.647
+        long cpf = 98765432109L; // Tipo "long" | Valor Máximo - > 9.223.372.036.854.775.807
+		float pi = 3.14F; // Tipo "float" | Valor Máximo - > 6 – 7 dígitos
+        double salarioMinimo = 2500.00; // Tipo "double" | Valor Máximo - > 15 dígitos
+    }
+}   
+
+
+//Os oito tipos primitivos em Java são: int, byte, short, long, float, double, boolean e char 
+
+// Observações
+
+// Para utilizar "long", precisar colocar a letra "L" no final
+// Para utilizar "float", precisar colocar a letra "F" no final
